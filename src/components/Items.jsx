7f8a92618eb82baeba0ef595909d35745b5d08ba
John Anthon Dela Cruz <155972599+devJohnA@users.noreply.github.com>
2025-12-11 13:02:20 +0800

    sadasd

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,1 +1,11 @@
+import React from "react";
 
+const Items = () => {
+  return (
+    <div className="header">
+      <span className="title">Word Hunt</span>
+    </div>
+  );
+};
+
+export default Items;
