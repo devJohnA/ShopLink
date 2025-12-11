410eaea3e6d066b403cdf774cd0fdaca2836208d
John Anthon Dela Cruz <155972599+devJohnA@users.noreply.github.com>
2025-12-12 07:17:13 +0800

    asd

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -17,7 +17,7 @@ const Items = () => {
             <img
               src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg"
               alt="Front of men's Basic Tee in black."
-              className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+              className="aspect-square w-full rounded-md bg-gray-200 object-cover hover:opacity-75 lg:aspect-auto lg:h-80"
             />
             <div className="mt-4 flex justify-between">
               <div>
