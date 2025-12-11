632a5d76743186b41271edf69cab87746662c7af
John Anthon Dela Cruz <155972599+devJohnA@users.noreply.github.com>
2025-12-12 06:15:13 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar.jsx";
-// import Home from "./components/Home.jsx";
+import Home from "./components/Home.jsx";
 // import Items from "./components/Items.jsx";
 // import Footer from "./components/Footer.jsx";
 function App() {
   return (
     <>
       <Navbar />
-      {/* <Home />
-     <Items /> */}
-      {/* <Footer /> */}
+      <Home />
+     {/* <Items /> 
+      <Footer /> */}
     </>
   );
 }
