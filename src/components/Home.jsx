74e8815f15d5c2992cc02bc0a65db6291ad54416
John Anthon Dela Cruz <155972599+devJohnA@users.noreply.github.com>
2025-12-11 09:55:57 +0800

    asd

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,16 +22,6 @@ const Home = () => {
 
         {/* Main Content */}
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
-              Announcing our next round of funding.{" "}
-              <a href="#" className="font-semibold text-indigo-400">
-                <span aria-hidden="true" className="absolute inset-0"></span>
-                Read more <span aria-hidden="true">→</span>
-              </a>
-            </div>
-          </div>
-
           <div className="text-center">
             <h1 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl">
               Data to enrich your online business
@@ -42,7 +32,7 @@ const Home = () => {
               lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.
             </p>
 
-            <div className="mt-10 flex items-center justify-center gap-x-6">
+            <div className="mt-10 flex items-center justify-center gap-x-8">
               <a
                 href="#"
                 className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
@@ -76,6 +66,7 @@ const Home = () => {
 };
 
 export default Home;
+
 
 
 
