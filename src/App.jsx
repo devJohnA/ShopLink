f84fe2902e8e7d95f67af04faf5102cad64ac1f3
John Anthon Dela Cruz <155972599+devJohnA@users.noreply.github.com>
2025-12-11 19:48:26 +0800

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import NavBar from "./components/Navbar.jsx";
+import Navbar from "./components/Navbar.jsx";
 import Home from "./components/Home.jsx";
 import Items from "./components/Items.jsx";
 // import Footer from "./components/Footer.jsx";
 function App() {
   return (
     <>
-      <NavBar />
+      <Navbar />
       <Home />
      <Items />
       {/* <Footer /> */}
