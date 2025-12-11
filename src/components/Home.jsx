af79980e1d63ae4c78a795ff980dd93d0cd38fb8
John Anthon Dela Cruz <155972599+devJohnA@users.noreply.github.com>
2025-12-11 09:29:15 +0800

    asd

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+// Source - https://stackoverflow.com/q
+// Posted by Jacques Jacob, modified by community. See post 'Timeline' for change history
+// Retrieved 2025-12-11, License - CC BY-SA 4.0
+
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="header">
+      <span className="title">Word Hunt</span>
+    </div>
+  );
+};
+
+export default Home;
