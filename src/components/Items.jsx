bfc2f9ceba0a4025921caf9a2f247d31efcc77e7
John Anthon Dela Cruz <155972599+devJohnA@users.noreply.github.com>
2025-12-11 13:12:13 +0800

    sad

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -95,6 +95,126 @@ const Items = () => {
             </div>
           </div>
 
+          <div className="group relative">
+            <img
+              src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-04.jpg"
+              alt="Front of men's Artwork Tee in peach."
+              className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+            />
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    Artwork Tee
+                  </a>
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">Iso Dots</p>
+              </div>
+              <p className="text-sm font-medium text-gray-900">$35</p>
+            </div>
+          </div>
+
+          <div className="group relative">
+            <img
+              src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-04.jpg"
+              alt="Front of men's Artwork Tee in peach."
+              className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+            />
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    Artwork Tee
+                  </a>
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">Iso Dots</p>
+              </div>
+              <p className="text-sm font-medium text-gray-900">$35</p>
+            </div>
+          </div>
+
+          <div className="group relative">
+            <img
+              src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-04.jpg"
+              alt="Front of men's Artwork Tee in peach."
+              className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+            />
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    Artwork Tee
+                  </a>
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">Iso Dots</p>
+              </div>
+              <p className="text-sm font-medium text-gray-900">$35</p>
+            </div>
+          </div>
+
+          <div className="group relative">
+            <img
+              src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-04.jpg"
+              alt="Front of men's Artwork Tee in peach."
+              className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+            />
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    Artwork Tee
+                  </a>
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">Iso Dots</p>
+              </div>
+              <p className="text-sm font-medium text-gray-900">$35</p>
+            </div>
+          </div>
+
+          <div className="group relative">
+            <img
+              src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-04.jpg"
+              alt="Front of men's Artwork Tee in peach."
+              className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+            />
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    Artwork Tee
+                  </a>
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">Iso Dots</p>
+              </div>
+              <p className="text-sm font-medium text-gray-900">$35</p>
+            </div>
+          </div>
+
+          <div className="group relative">
+            <img
+              src="https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-04.jpg"
+              alt="Front of men's Artwork Tee in peach."
+              className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+            />
+            <div className="mt-4 flex justify-between">
+              <div>
+                <h3 className="text-sm text-gray-700">
+                  <a href="#">
+                    <span aria-hidden="true" className="absolute inset-0"></span>
+                    Artwork Tee
+                  </a>
+                </h3>
+                <p className="mt-1 text-sm text-gray-500">Iso Dots</p>
+              </div>
+              <p className="text-sm font-medium text-gray-900">$35</p>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
