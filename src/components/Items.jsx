51f4ec0f17a307cde8425c04411fad12476d14d5
John Anthon Dela Cruz <155972599+devJohnA@users.noreply.github.com>
2025-12-11 13:27:29 +0800

    Replace 'Buy Now' button with icon in Items component

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -31,7 +31,7 @@ const Items = () => {
               </div>
               <p className="text-sm font-medium text-gray-900">$35</p>
              <div> 
-              <a href="#" className="bg-[#054C73] rounded text-sm w-9 h-15 px-5 py-2 text-white font-bold">Buy Now</a>
+              <a href="#" className="bg-[#054C73] rounded text-sm w-9 h-15 px-5 py-2 text-white font-bold"><HiArrowCircleRight /></a>
              </div>
             
             </div>
@@ -81,7 +81,7 @@ const Items = () => {
               <p className="text-sm font-medium text-gray-900">$35</p>
              <div> 
               
-              <a href="#" className="bg-[#054C73] rounded text-sm w-9 h-15 px-5 py-2 text-white font-bold"><HiArrowCircleRight size="24px" /> </a>
+              <a href="#" className="bg-[#054C73] rounded text-sm w-9 h-15 px-5 py-2 text-white font-bold">Buy Now </a>
              </div>
             </div>
           </div>
