99098ac4a9522abbaa78978b483704bf9eec9a1d
John Anthon Dela Cruz <155972599+devJohnA@users.noreply.github.com>
2025-12-11 13:36:33 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "../App.css";
 const Footer = () => {
   return (
     <div className="header">
-      <span className="title">Word Hunt</span>
+      <span className="flex items-center justify-center h-48 bg-gray-200">Word Hunt</span>
     </div>
   );
 };
