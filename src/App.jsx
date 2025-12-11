f28c68be0dd5aa05ebdc3ecbb57542d53e3087b6
John Anthon Dela Cruz <155972599+devJohnA@users.noreply.github.com>
2025-12-11 13:03:07 +0800

    Import Item component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import NavBar from "./components/Navbar.jsx";
 import Home from "./components/Home.jsx";
-
+import Item from "./components/Item.jsx";
 function App() {
   return (
     <>
