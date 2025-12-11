14a69a6b78be76ed55ddf73e0cb851c3e228850a
John Anthon Dela Cruz <155972599+devJohnA@users.noreply.github.com>
2025-12-11 13:35:41 +0800

    asd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,11 @@
+import React from "react";
 
+const Footer = () => {
+  return (
+    <div className="header">
+      <span className="title">Word Hunt</span>
+    </div>
+  );
+};
+
+export default Footer;
