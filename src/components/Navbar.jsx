6946270a5bd8b23ee24ff9aa20a509ed089bd91c
John Anthon <delacruzjohnanthon@gmail.com>
2025-12-15 21:41:50 +0800

    asd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -91,10 +91,7 @@ const Navbar = () => {
               Categories
             </li>
             <li className="mt-4">
-              <button
-                onClick={handleGetApp}
-                className="px-8 py-3 cursor-pointer font-semibold text-sm bg-[#054C73] rounded-full text-white transition-colors hover:bg-[#043c5a]"
-              >
+              <button onClick={handleGetApp} className="px-8 py-3 cursor-pointer font-semibold text-sm bg-[#054C73] rounded-full text-white transition-colors hover:bg-[#043c5a]">
                 Get the app
               </button>
             </li>
